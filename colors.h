d0ca62ef4d7fa3c3018544ebef45e4b16eec7f35
Andrey <github@spirit.re>
2020-03-28 08:39:15 +0300

    Gamma and compatibility updates

--- colors.h
+++ colors.h
@@ -2,14 +2,14 @@
 
 #define TRANSPARENT	0
 
-#define RGB(r, b, g) ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | ((b & 0xF8) >> 3)
+#define RGB(r, b, g) (((r & 0b11111000)<<8) + ((g & 0b11111100)<<3)+(b>>3))
 
 #define GRAY      RGB(127, 127, 127)
 #define DARKGRAY  RGB(64, 64, 64)
 #define LIGHTGRAY RGB(220, 220, 220)
 
 #define RED	      RGB(255,0,0)
-#define GREEN     RGB(0, 255, 0)
+#define GREEN     RGB(0, 100, 0)
 #define BLUE      RGB(0, 0, 255)
 
 #define ORANGE    RGB(255,140,0)
